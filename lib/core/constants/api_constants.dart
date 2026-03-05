a7d5c8573c1ae13948cb92c88d5f669741e92374
Jason <sanolamian12@gmail.com>
2026-03-05 22:28:37 +1100

    PlayStore denied (2nd) releasing - modified for http connection - delete 3000 port number

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,7 +1,9 @@
 class ApiConstants {
   // 실제 EC2 공인 IP 주소를 입력하세요.
   //static const String baseUrl = 'http://3.26.146.6:3000';
-  static const String baseUrl = 'http://13.210.139.139:3000';
+  //static const String baseUrl = 'http://13.210.139.139:3000';
+  static const String baseUrl = 'http://13.210.139.139';
+
 
   // Auth 관련 경로
   static const String login = '$baseUrl/auth/login';
